b9dcc014ccbea2dd6923ab946a1a5082e3da15a9
hadhiee <hadhiee@gmail.com>
2026-02-17 22:49:36 +0700

    Update manifest to include maskable icon purpose

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,6 +14,7 @@ export default function manifest(): MetadataRoute.Manifest {
                 src: '/icon.svg',
                 sizes: 'any',
                 type: 'image/svg+xml',
+                purpose: 'any maskable',
             },
         ],
     };
